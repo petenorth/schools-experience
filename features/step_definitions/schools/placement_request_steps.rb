bc379ceaeea0b4a5655d2dd8a0d4c8b88323252e
Peter Fry <3152937+petenorth@users.noreply.github.com>
2020-03-14 13:44:43 +0000

    Update placement_request_steps.rb

--- features/step_definitions/schools/placement_request_steps.rb
+++ features/step_definitions/schools/placement_request_steps.rb
@@ -120,8 +120,6 @@ Then("I should see a {string} section with the following values:") do |heading, 
           expect(page).to have_css('dd', text: /#{subject}/i)
         end
       else
-        puts row['value']
-        puts page.html
         expect(page).to have_css('dd', text: /#{row['Value']}/i)
       end
     end
