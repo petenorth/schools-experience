cba18a98eccb55c20951ade3f88cc87bf970ed30
Peter Fry <peterfry@peters-mbp-2.mynet>
2020-11-15 16:11:27 +0000

    installing the graalvm-ce distrbution

--- local-variable-trace.rb
+++ local-variable-trace.rb
@@ -1,12 +1,12 @@
 puts("Ruby: Insight version " + insight[:version] + " is launching")
 
-config = Truffle::Interop.hash_keys_as_members({
-  roots: true,
-  sourceFilter: -> (src) {
-    return src[:name].include? "capabilities.rb"
-  }
-})
+#config = Truffle::Interop.hash_keys_as_members({
+#  roots: true,
+#  sourceFilter: -> (src) {
+#    return src[:name].include? "capabilities.rb"
+#  }
+#})
 
 insight.on("enter", -> (ctx, frame) {
     puts("camel_case for " + frame[:n].to_s)
-}, config)
+})
